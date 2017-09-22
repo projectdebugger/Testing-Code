ee70e33c81c854ec2afe9e828c64ea5c8f7d718e
kanavbadwal <kanavbadwal@gmail.com>
2017-09-22 14:11:17 +1000

    Test file is created for ticketinserted function

---
+++ bcccp/src/bcccp/testing/Ticketinserted.java
@@ -0,0 +1,17 @@
+/*
+*This file is created to test tic
+*/
+package bcccp.testing;
+
+import bcccp.carpark.*;
+/* Author: HARKANAV BIR SINGH BADWAL
+ +   	 * Unit Testing TICKET INSERTED
+ +	 * */
+public class Ticketinserted {
+    
+    public static void main(String[] args){
+        
+    }
+    
+    
+}
