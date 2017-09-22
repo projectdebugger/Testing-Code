b4a65f48d258a738421ee82586dc234c6314e8c6
kanavbadwal <kanavbadwal@gmail.com>
2017-09-22 14:14:36 +1000

    Updation in ticketinserted test file.

--- bcccp/src/bcccp/testing/Ticketinserted.java
+++ bcccp/src/bcccp/testing/Ticketinserted.java
@@ -1,16 +1,35 @@
 /*
-*This file is created to test tic
+*This file is created to test ticketinserted function.
 */
 package bcccp.testing;
 
-import bcccp.carpark.*;
+import bcccp.carpark.paystation.IPaystationController;
 /* Author: HARKANAV BIR SINGH BADWAL
  +   	 * Unit Testing TICKET INSERTED
  +	 * */
 public class Ticketinserted {
     
     public static void main(String[] args){
+        IPaystationController ti;
+        ti = new IPaystationController() {
+            
+            @Override
+            public void ticketInserted(String barcode) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void ticketPaid() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void ticketTaken() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
         
+        ti.ticketInserted("A1");
     }
     
     
