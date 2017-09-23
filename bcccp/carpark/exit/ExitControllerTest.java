5c3b977023b715c5ff0f3f31331934514d3a6909
santoshbista02 <santoshbista02@gmail.com>
2017-09-23 17:59:40 +1000

    Exit Controller

--- bcccp/carpark/exit/ExitControllerTest.java
+++ bcccp/carpark/exit/ExitControllerTest.java
@@ -1,65 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package carpark.exit;
 
 import bcccp.carpark.exit.ExitController;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
- * @author android
+ * @author Santosh
  */
 public class ExitControllerTest {
-    
-    public ExitControllerTest() {
+ 
+    public static void main(String [] args){
+        
+        ExitController exit=new ExitController();
+        
+        //13th commit test carEventDetected
+        exit.carEventDetected("S1111", true);
+        
+        //14th commit test carEventDetected and set state as idle
+        exit.carEventDetected("S1111", true);
+        
+        
+        
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of carEventDetected method, of class ExitController.
-     */
-    @Test
-    public void testCarEventDetected() {
-        System.out.println("carEventDetected");
-        String detectorId = "";
-        boolean carDetected = false;
-        ExitController instance = null;
-        instance.carEventDetected(detectorId, carDetected);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ticketInserted method, of class ExitController.
-     */
-    @Test
-    public void testTicketInserted() {
-        System.out.println("ticketInserted");
-        String ticketStr = "";
-        ExitController instance = null;
-        instance.ticketInserted(ticketStr);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    
-    
 }
