9721e4b8178e26464c40c29f371f7fc76d5570fd
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-25 21:02:33 +1000

    Unit testing
    Throwing an exception when season ticket id is null

--- bcccp/tickets/season/TestSeasonTicket.java
+++ bcccp/tickets/season/TestSeasonTicket.java
@@ -13,7 +13,10 @@ public class TestSeasonTicket {
        SeasonTicket ticket1 = new SeasonTicket("S0011","CP3",12022017,120320170 );
        System.out.println("Unique Season Ticket Id is: Season Ticket Id: "+ ticket1.getId()+" Season ticket carpark name:"+ticket1.getCarparkId()+" Valid start date "+ticket1.getStartValidPeriod()+" valid end date: "+ticket1.getEndValidPeriod());
 
-       
+       System.out.println("Throwing an exception when season ticket id is null ");
+       SeasonTicket ticket2 = new SeasonTicket("","CP3",12022017,120320170);
+       System.out.println("Unique Season Ticket Id is: "+ ticket2.getId()+" Season ticket carpark name:"+ticket2.getCarparkId()+" Valid start date "+ticket2.getStartValidPeriod()+" valid end date: "+ticket2.getEndValidPeriod());
+        
 
     }
 }
