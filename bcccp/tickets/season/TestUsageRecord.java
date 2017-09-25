6ee85cc91aeb322efaf7a9dd7f0d37008390b4a5
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-25 21:53:00 +1000

    Unit Testing
    Throwing an exception when season ticket id is null

--- bcccp/tickets/season/TestUsageRecord.java
+++ bcccp/tickets/season/TestUsageRecord.java
@@ -10,7 +10,10 @@ public class TestUsageRecord {
         /** Creating the instance of Usage Record**/
            UsageRecord ticket1 = new UsageRecord("S0011", 1203201703 );
            System.out.println("Unique Season Usage Record is: :  Ticket number "+ ticket1.getSeasonTicketId()+" Season ticket Valid start date and time "+ticket1.getStartTime());
-
+	   System.out.println("Throwing an exception when season ticket id is null ");
+           UsageRecord ticket2 = new UsageRecord("", 120320170);
+           System.out.println("Unique Season Ticket Id is: "+ ticket2.getSeasonTicketId()+" Valid start date "+ticket2.getStartTime());
+           
         
          }
 }
