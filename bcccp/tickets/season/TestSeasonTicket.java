1dd7d0450998f082c3973ed9f37fc85a6e47b908
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-25 21:11:35 +1000

    unit testing
    rowing an exception when Start Date is less than or equal to 0

--- bcccp/tickets/season/TestSeasonTicket.java
+++ bcccp/tickets/season/TestSeasonTicket.java
@@ -16,6 +16,13 @@ public class TestSeasonTicket {
        System.out.println("Throwing an exception when season ticket id is null ");
        SeasonTicket ticket2 = new SeasonTicket("","CP3",12022017,120320170);
        System.out.println("Unique Season Ticket Id is: "+ ticket2.getId()+" Season ticket carpark name:"+ticket2.getCarparkId()+" Valid start date "+ticket2.getStartValidPeriod()+" valid end date: "+ticket2.getEndValidPeriod());
+       System.out.println("Throwing an exception when carpark name is null ");
+       SeasonTicket ticket5 = new SeasonTicket("S00112","",12022017,120320170);
+       System.out.println("Unique CarParkId is: "+ ticket5.getCarparkId());
+       System.out.println("Throwing an exception when Start Date is less than or equal to zero.");
+       SeasonTicket ticket3 = new SeasonTicket("S0011","CP3",0,120320170 ); 
+       System.out.println("Your Ticket Start at : "+ ticket3.getStartValidPeriod());
+         
         
 
     }
